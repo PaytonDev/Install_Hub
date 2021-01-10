3177863ce8fc3d6f9a1a7e07eb3efa67e415becf
Breeana Payton <breeana.payton@gmail.com>
2021-01-09 23:57:45 -0500

    Correction on app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///install'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config["SECRET_KEY"] = "super-extra-secretive"
+
 
 connect_db(app)
