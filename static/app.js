f5c9de8b63afa809218eb3aabba34256c73aabef
Breeana Payton <breeana.payton@gmail.com>
2021-03-03 09:55:34 -0500

    Updated the Login Page

--- static/app.js
+++ static/app.js
@@ -1,5 +1,13 @@
-const notes = new Object
-let numNotes = Object.keys(notes).length
+// Create array of employees
+$('.dropdown').on('click', "a", function(e) {
+    e.preventDefault()
+    let emps = [];
+    let empobj = new Object; 
+    $('.employees-int').each( function(i, val) {
+        emps.push($(val).text());
+        return emps;
+    })
+})
 
 
 $('.employees-int').on("click", function(e) {
@@ -8,8 +16,8 @@ $('.employees-int').on("click", function(e) {
     if ($empName) {
         $('#employee-name-title').text('');
         $('#employee-name-title').append(e.target.innerText);
+
     } else {
-        console.log(e)
         $('#employee-name-title').append(e.target.innerText);
     }
     $('#emp-info').removeClass('d-none');
@@ -24,7 +32,6 @@ $('.int-action').on('click', function(e) {
         $notesHead.text('')
         $notesHead.text(e.target.innerText)
     } else {
-        console.log(e)
         $notesHead.append(e.target.innerText)
     }
 })
@@ -32,17 +39,43 @@ $('.int-action').on('click', function(e) {
 // Adding a timestamped interaction and correlating note 
 $('#add-note').on('click', function(e) {
     e.preventDefault();
-    let $noteContent = $('#note-content').text();
+    let noteContent = $.trim($('textarea').val());
     let $notesHead = $('#notes-header').text();
-    let timestamp = moment().format("MMM Do, h:mm a")
-    $('.interaction-list').append(`<li><a href="#" id="note-${numNotes}" class="note-link text-decoration-none">${$notesHead} on ${timestamp}</a></li>`);
-    notes[numNotes] = $noteContent;
+    let timestamp = moment().format("MMM Do, h:mm a");
+    let numNotes = Object.keys(notes).length
+
+    if (noteContent != '') {
+    $('.interaction-list').append(`<li class="note-link text-decoration-none" id="note-${numNotes}" >${$notesHead} on ${timestamp}</a></li>`);
+    notes[`note-${numNotes}`] = noteContent;
+    console.log(noteContent)
+    return notes
+    } else {
+        alert('Please enter note!')
+    }
 })
 
 
-$('.note-link').on('click', function(e) {
-    e.preventDefault();
-    $('#note-display').text(notes[e.target.attribute.id]);
-    console.log(notes)
-    $('.notes-area').toggleClass("d-none");
-})
+$('.interaction-list').on('click', 'li', function(e) {
+    e.preventDefault()
+    console.log(e)
+    $('#note-display').text(notes[e.target.id]);
+    $('.notes-area').removeClass("d-none");
+});
+
+setInterval(function time(){
+    let d = new Date();
+    let hours = 24 - d.getHours();
+    let min = 60 - d.getMinutes();
+    let sec = 60 - d.getSeconds();
+
+    if ((min + '').length == 1){
+        min = "0" + min;
+    }
+
+    if ((sec + '').length == 1){
+        sec = "0" + sec;
+    }
+
+    $('#covid-countdown').text(`${hours}hrs ${min}mins ${sec}s`)
+
+}, 1000);
