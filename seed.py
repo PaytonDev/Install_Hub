7c78148991f9c00bc8d8869aa9e89e142e6df111
Breeana Payton <breeana.payton@gmail.com>
2021-03-22 15:21:16 -0400

    Added Interaction Note Functionality

--- seed.py
+++ seed.py
@@ -1,21 +1,21 @@
-# from models import Department, Employee, Installer, Task, Dealership, Project, db
-# from app import app
-# import random
-# import string
+from models import Department, Employee, User, Task, Dealership, Project, db
+from app import app
+import random
+import string
 
-# db.drop_all()
-# db.create_all()
+db.drop_all()
+db.create_all()
 
-# d1 = Department(department="Accounting")
-# d2 = Department(department="Business Office")
-# d3 = Department(department="Parts")
-# d4 = Department(department="Service")
+d1 = Department(department="Accounting")
+d2 = Department(department="Business Office")
+d3 = Department(department="Parts")
+d4 = Department(department="Service")
 
-# e1 = Employee(e_firstname="Jennifer", e_lastname="Finch", e_department=1, e_title="Controller", phone="555-555-5555", email="jen.finch@janeford.com")
-# e2 = Employee(e_firstname="Thadeus", e_lastname="Gathercoal", e_department=2, e_title="Sales Manager", phone="777-777-77777", email="thad.gathercoal@janeford.com'")
-# e3 = Employee(e_firstname="Sonja", e_lastname="Pauley", e_department=3, e_title="Parts Manager", phone="888-888-8888", email="sonja.pauley@janeford.com")
-# e4 = Employee(e_firstname="Waneta", e_lastname="Skeleton", e_department=4, e_title="Service Manager", phone="999-999-9999", email="waneta.skeleton@janeford.com")
-# e5 = Employee(e_firstname="Cohen", e_lastname="Norton", e_department=1, e_title="Controller", phone="963-852-8527", email="cohen.norton@johnnissan.com")
-# e6 = Employee(e_firstname="Loretta", e_lastname="Elwyn", e_department=2, e_title="", phone="", email="")
-# e7 = Employee(e_firstname="", e_lastname="", e_department=3, e_title="", phone="", email="")
-# e8 = Employee(e_firstname="", e_lastname="", e_department=4, e_title="", phone="", email="")
+e1 = Employee(e_firstname="Jennifer", e_lastname="Finch", e_department=1, e_title="Controller", phone="555-555-5555", email="jen.finch@janeford.com")
+e2 = Employee(e_firstname="Thadeus", e_lastname="Gathercoal", e_department=2, e_title="Sales Manager", phone="777-777-77777", email="thad.gathercoal@janeford.com'")
+e3 = Employee(e_firstname="Sonja", e_lastname="Pauley", e_department=3, e_title="Parts Manager", phone="888-888-8888", email="sonja.pauley@janeford.com")
+e4 = Employee(e_firstname="Waneta", e_lastname="Skeleton", e_department=4, e_title="Service Manager", phone="999-999-9999", email="waneta.skeleton@janeford.com")
+e5 = Employee(e_firstname="Cohen", e_lastname="Norton", e_department=1, e_title="Controller", phone="963-852-8527", email="cohen.norton@johnnissan.com")
+e6 = Employee(e_firstname="Loretta", e_lastname="Elwyn", e_department=2, e_title="", phone="", email="")
+e7 = Employee(e_firstname="", e_lastname="", e_department=3, e_title="", phone="", email="")
+e8 = Employee(e_firstname="", e_lastname="", e_department=4, e_title="", phone="", email="")
--- static/app.js
+++ static/app.js
@@ -1,29 +1,52 @@
-// Create array of employees
-$('.dropdown').on('click', "a", function(e) {
-    e.preventDefault()
+let currentEmp = '';
+let empNoteList = new Object;
+
+
+function arrayOfEmployees() {
     let emps = [];
-    let empobj = new Object; 
     $('.employees-int').each( function(i, val) {
         emps.push($(val).text());
-        return emps;
     })
+    return emps;
+}
+
+// Create array of employees for employee dropdown selection on interaction page
+$('.dropdown').on('click', "a", function(e) {
+    e.preventDefault()
+    arrayOfEmployees()
 })
 
+// function for creating an object to hold interactions for each employee
+function createNotesObject() {
+    let emps = arrayOfEmployees()
+    let keys = Object.keys(empNoteList)
+    if (keys.length === 0) {
+        for (let e of emps) {
+            empNoteList[e] = [];
+            return empNoteList
+        }
+    } else {
+        return empNoteList;
+    }
+}
 
+// Displaying employee name on interaction page
 $('.employees-int').on("click", function(e) {
     e.preventDefault();
-    let $empName = $('#employee-name-title').text()
-    if ($empName) {
+    let $empNameTitle = $('#employee-name-title').text()
+    let empName = e.target.innerText;
+    if ($empNameTitle) {
         $('#employee-name-title').text('');
-        $('#employee-name-title').append(e.target.innerText);
-
+        $('#employee-name-title').append(empName);
+        currentEmp = empName;
     } else {
-        $('#employee-name-title').append(e.target.innerText);
+        $('#employee-name-title').append(empName);
+        currentEmp = empName;
     }
     $('#emp-info').removeClass('d-none');
-
 })
 
+// Adding notes header with type of interaction
 $('.int-action').on('click', function(e) {
     e.preventDefault();
     let $notesHead = $('#notes-header')
@@ -36,32 +59,37 @@ $('.int-action').on('click', function(e) {
     }
 })
 
-// Adding a timestamped interaction and correlating note 
+// Adding a timestamped interaction link that leads to correlating note 
 $('#add-note').on('click', function(e) {
     e.preventDefault();
+    let notes = createNotesObject()
     let noteContent = $.trim($('textarea').val());
     let $notesHead = $('#notes-header').text();
     let timestamp = moment().format("MMM Do, h:mm a");
-    let numNotes = Object.keys(notes).length
 
     if (noteContent != '') {
-    $('.interaction-list').append(`<li class="note-link text-decoration-none" id="note-${numNotes}" >${$notesHead} on ${timestamp}</a></li>`);
-    notes[`note-${numNotes}`] = noteContent;
-    console.log(noteContent)
-    return notes
+        notes[currentEmp].push(noteContent)
+
+        $('.interaction-list').append(
+            `<li class="note-link text-decoration-none"
+            id="note-${notes[currentEmp].indexOf(noteContent)}" >
+            <a>${$notesHead} on ${timestamp}</a></li>`
+            );
+
     } else {
         alert('Please enter note!')
     }
 })
 
-
+// displaying the interaction link
 $('.interaction-list').on('click', 'li', function(e) {
     e.preventDefault()
-    console.log(e)
-    $('#note-display').text(notes[e.target.id]);
+    let notes = createNotesObject();
+    $('#note-display').text(notes[currentEmp]);
     $('.notes-area').removeClass("d-none");
 });
 
+// Covid Countdown Timer
 setInterval(function time(){
     let d = new Date();
     let hours = 24 - d.getHours();
